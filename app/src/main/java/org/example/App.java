8d439f7ded27a5c54b4d1454c55540334423b86c
MrKylox <93052940+MrKylox@users.noreply.github.com>
2024-06-13 08:17:06 -0500

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,49 +1,74 @@
 package org.example;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("CSV Exporter");
+        primaryStage.setTitle("Teams-Shifts Hub");
+        VBox main_vbox = new VBox();
+        main_vbox.setPadding( new Insets( 10 ));
+        main_vbox.setBackground(
+            new Background(
+                new BackgroundFill(Color.BLACK, new CornerRadii(0), new Insets(0))
+            ));
 
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+        Pane p = new Pane();
 
-        Button exportButton = new Button("Export to CSV");
-        exportButton.setOnAction(e -> {
-            File file = fileChooser.showSaveDialog(primaryStage);
-            if (file != null) {
-                exportToCSV(file);
-            }
-        });
+        Arc largeArc = new Arc(0, 0, 100, 100, 270, 90);
+        largeArc.setFill(Color.web("0x59291E"));
+        largeArc.setType(ArcType.ROUND);
 
-        // primaryStage.initStyle(StageStyle.DECORATED);
-        // VBox vBox = new VBox(exportButton);
-        // Scene scene = new Scene(vBox, 600, 600);
-        // primaryStage.setScene(scene);
-        Scene scene = new Scene(createContent());
+        Arc backgroundArc = new Arc(0, 0, 160, 160, 270, 90);
+        backgroundArc.setFill( Color.web("0xD96F32"));
+        backgroundArc.setType( ArcType.ROUND);
+
+        Arc smArc1 = new Arc(0, 160, 30, 30, 270, 180);
+        smArc1.setFill(Color.web("0xF2A444"));
+        smArc1.setType(ArcType.ROUND);
+
+        Circle smCircle = new Circle(
+            160/Math.sqrt(2.0), 160/Math.sqrt(2.0), 30, Color.web("0xF2A444")
+        );
+
+        Arc smArc2 = new Arc(160, 0, 30, 30, 180, 180);
+        smArc2.setFill(Color.web("0xF2A444"));
+        smArc2.setType(ArcType.ROUND);
+
+        //9568.20: X, 320.20:Y
+        Arc medArc = new Arc(1380, 320.20, 60, 60, 90, 90);
+        medArc.setFill(Color.web("0xD9583B"));
+        medArc.setType(ArcType.ROUND);
+
+        p.getChildren().addAll(backgroundArc, largeArc, smArc1, smCircle, smArc2, medArc);
+
+        main_vbox.getChildren().add(p);
+
+        Scene scene = new Scene(main_vbox);
+        scene.setFill(Color.BLACK);
+
+        primaryStage.setTitle("Pane App");
         primaryStage.setScene(scene);
-
-        primaryStage.setOnCloseRequest(event -> {System.out.println("Closing Stage");});
-
+        primaryStage.setWidth(568);
+        primaryStage.setHeight(320);
         primaryStage.show();
-
         
     }
 
@@ -51,6 +76,9 @@ public class App extends Application {
         ValidatingTextField field = new ValidatingTextField(input -> input.contains("a"));
         field.setFont(Font.font(22));
         Button btn = new Button("Submit");
+        btn.setOnAction(event -> {
+            System.out.println("button Was pressed");
+        });
         btn.setFont(Font.font(22));
         btn.disableProperty().bind(field.isValidProperty().not());
 
@@ -61,30 +89,30 @@ public class App extends Application {
 
 
 
-    private void exportToCSV(File file) {
-        String[] headers = { "ID", "Name", "Email" };
-        String[][] data = {
-                { "1", "John Doe", "john.doe@example.com" },
-                { "2", "Jane Smith", "jane.smith@example.com" },
-                { "3", "Mike Johnson", "mike.johnson@example.com" }
-        };
+    // private void exportToCSV(File file) {
+    //     String[] headers = { "ID", "Name", "Email" };
+    //     String[][] data = {
+    //             { "1", "John Doe", "john.doe@example.com" },
+    //             { "2", "Jane Smith", "jane.smith@example.com" },
+    //             { "3", "Mike Johnson", "mike.johnson@example.com" }
+    //     };
 
-        CSVFormat csvFormat = CSVFormat.DEFAULT.builder().setHeader(headers).build();
+    //     CSVFormat csvFormat = CSVFormat.DEFAULT.builder().setHeader(headers).build();
 
-        try (FileWriter writer = new FileWriter(file);
-             CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat)) {
+    //     try (FileWriter writer = new FileWriter(file);
+    //          CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat)) {
 
-            for (String[] record : data) {
-                csvPrinter.printRecord((Object[]) record);
-            }
+    //         for (String[] record : data) {
+    //             csvPrinter.printRecord((Object[]) record);
+    //         }
 
-            csvPrinter.flush();
-            System.out.println("CSV file created successfully!");
+    //         csvPrinter.flush();
+    //         System.out.println("CSV file created successfully!");
 
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+    //     } catch (IOException ex) {
+    //         ex.printStackTrace();
+    //     }
+    // }
 
     public static void main(String[] args) {
         launch(args);
